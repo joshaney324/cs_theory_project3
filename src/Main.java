8af4121016f7fab853111e11755719b47cfd8d2f
Josh Aney <josh.aney@icloud.com>
2024-11-17 20:40:54 -0700

    Set up

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Graph graph = new Graph("Graphs\\graph1.txt");
+        System.out.println("Test");
+    }
+
+    public static void exactVC(Graph graph) {
+
+    }
+
+    public static void inexactVC(Graph graph) {
+
+    }
+
+    public static void exactIS(Graph graph) {
+
+    }
+
+    public static void inexactIS(Graph graph) {
+
+    }
+}
