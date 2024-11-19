6bb08168193de60759409edf5ba9b5513077585b
Nathan <nsrgy22@gmail.com>
2024-11-19 16:43:54 -0700

    hughpid

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
         Graph graph = new Graph("Graphs\\graph1.txt");
         System.out.println("Test");
+        System.out.println("HUGH IS STUPID");
     }
 
     public static void exactVC(Graph graph) {
+
+
 
     }
 
