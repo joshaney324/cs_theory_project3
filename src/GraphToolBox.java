173029f84652b2c8d4d035a2fe3d6d64a83d48a2
Josh Aney <josh.aney@icloud.com>
2024-11-20 18:33:20 -0700

    moved to tool box as specified in the rubric

--- src/GraphToolBox.java
+++ src/GraphToolBox.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 /**
  *
@@ -5,22 +8,232 @@
  */
 public class GraphToolBox {
     // return an array containing the vertex numbers of an optimal VC.
-    public static int[] exactVC(Graph inputGraph) {
-        return null;
+    public static List<Integer> exactVC(Graph graph) {
+        int[] vertexList = optimalIS(graph);
+        List<Integer> bestIS = new ArrayList<>();
+        for (int i : vertexList) {
+            bestIS.add(i);
+        }
+        int[][] aGraph = graph.getGraph();
+        List<Integer> bestVC = new ArrayList<>();
+        for (int i = 0; i < aGraph.length; i++) {
+            if (!bestIS.contains(i)) {
+                bestVC.add(i);
+            }
+        }
+        return bestVC;
     }
-    
+
+
     // return (in polynomial time) an array containing the vertex numbers of a VC.
-    public static int[] inexactVC(Graph inputGraph) {
-        return null;
+    public static int inexactVC(Graph g) {
+        int[][] graph = g.getGraph();
+        Dictionary<Integer, int[]> dict = new Hashtable<>();
+        List<Integer> tabooList = new ArrayList<Integer>();
+        for (int i = 0; i < graph.length; i++) {
+            dict.put(i, graph[i]);
+        }
+        int max = 0;
+        int startingNode = 0;
+        List<Integer> vc = new ArrayList<Integer>();
+        while (dict.size() > 0) {
+            max = 0;
+            for (int i = 0; i < graph.length; i++) {
+                if (graph[i].length > max && !tabooList.contains(i)) {
+                    startingNode = i;
+                    max = graph[i].length;
+                }
+            }
+            vc.add(startingNode);
+            tabooList.add(startingNode);
+            dict.remove(startingNode);
+            for (int i = 0; i < graph[startingNode].length; i++) {
+                if (!vc.contains(graph[startingNode][i])) {
+                    tabooList.add(graph[startingNode][i]);
+                    dict.remove(graph[startingNode][i]);
+                }
+            }
+        }
+
+
+        if (verifierVC(g, vc)) {
+            return vc.size();
+        } else {
+            System.out.println("ERROR ERROR");
+            return 0;
+        }
     }
     
     // return an array containing the vertex numbers of an optimal IS.
     public static int[] optimalIS(Graph inputGraph) {
-        return null;
+        int bestIS = 0;
+        List<Integer> bestSet = new ArrayList<>();
+        List<Integer> nodeList = new ArrayList<>();
+        int[][] actualGraph = inputGraph.getGraph();
+        int counter = 0;
+        for (int[] node : actualGraph) {
+            nodeList.add(counter);
+            counter++;
+        }
+
+        List<List<Integer>> powerSet = powerset(nodeList);
+
+
+        for (List<Integer> set : powerSet) {
+            if (verifierIS(inputGraph, set)) {
+                if (set.size() > bestIS) {
+                    bestIS = set.size();
+                    bestSet = set;
+//                    System.out.println(set.toString());
+                }
+            }
+        }
+
+        int[] vertexLabels = new int[bestSet.size()];
+        int i = 0;
+        for (Integer value : bestSet) {
+            vertexLabels[i++] = value;
+        }
+
+        return vertexLabels;
     }
     
     // return (in polynomial time) an array containing the vertex numbers of a IS.
     public static int[] inexactIS(Graph inputGraph) {
-        return null;
+        int[][] joshylisty = inputGraph.getGraph();
+        int numofjoshy = joshylisty.length;
+        boolean[] coveredquestionmark = new boolean[numofjoshy];
+        List<Integer> IS = new ArrayList<>();
+        for (int i = 0; i < numofjoshy; i++) {
+            if (!coveredquestionmark[i]) {
+                IS.add(i);
+                for (int friend : joshylisty[i]) {
+                    coveredquestionmark[friend] = true;
+                }
+            }
+        }
+        if (verifierIS(inputGraph, IS)) {
+            int[] vertexLabels = new int[IS.size()];
+            int i = 0;
+            for (Integer value : IS) {
+                vertexLabels[i++] = value;
+            }
+
+            return vertexLabels;
+            //            return IS.size();
+        } else {
+            System.out.println("ERROR ERROR");
+            return null;
+        }
+    }
+
+    public static List<List<Integer>> powerset(List<Integer> set) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for (int num : set) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+
+        return result;
+    }
+
+
+    public static boolean verifierIS(Graph graph, List<Integer> set) {
+        int[][] actualGraph = graph.getGraph();
+
+        for (int vertex : set) {
+            int numNeighbors = actualGraph[vertex].length;
+
+            for (int j = 0; j < numNeighbors; j++) {
+                int neighbor = actualGraph[vertex][j];
+
+                if (set.contains(neighbor)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean verifierVC(Graph g, List<Integer> set) {
+        int[][] graph = g.getGraph();
+        List<Integer> coveredNodes = new ArrayList<Integer>();
+        for (int vertex : set) {
+            if (!coveredNodes.contains(vertex)) {
+                coveredNodes.add(vertex);
+            }
+            for (int linkedNode : graph[vertex]) {
+                if (!coveredNodes.contains(linkedNode)) {
+                    coveredNodes.add(linkedNode);
+                }
+            }
+        }
+        if (coveredNodes.size() == graph.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void generateRandomGraph(int numNodes) {
+        Random random = new Random();
+        String fileName = "Graphs\\trialGraph.txt";
+
+        Map<Integer, List<Integer>> allEdges = new HashMap<>();
+
+        for (int i = 0; i < numNodes; i++) {
+            allEdges.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < numNodes; i++) {
+
+            for (int j = 0; j < numNodes; j++) {
+                if (i != j && random.nextDouble() < 0.4) {
+                    List<Integer> nodeIList = allEdges.get(i);
+
+                    if(!nodeIList.contains(j)) {
+                        nodeIList.add(j);
+                    }
+
+                    List<Integer> nodeJList = allEdges.get(j);
+
+                    if(!nodeJList.contains(i)) {
+                        nodeJList.add(i);
+                    }
+
+                    allEdges.put(i, nodeIList);
+                    allEdges.put(j, nodeJList);
+                }
+            }
+        }
+
+        try (FileWriter aFile = new FileWriter(fileName)) {
+            aFile.write("numVert " + numNodes + "\n");
+            aFile.write("vertex numNeighbors neighbors\n");
+
+            for (int x = 0; x < numNodes; x++) {
+                StringBuilder line = new StringBuilder(x + " ");
+                int numNeighbors = allEdges.get(x).size();
+
+                line.append(numNeighbors).append(" ");
+                for (int neighbor : allEdges.get(x)) {
+                    line.append(neighbor).append(" ");
+                }
+
+                line.append("\n");
+                aFile.write(line.toString());
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error writing the graph to file: " + e.getMessage());
+        }
     }
 }
